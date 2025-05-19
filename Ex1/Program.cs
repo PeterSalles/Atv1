ffc1aa62a49605c5ed234c08a0ea465e57331daa
PeterSalles <101407569+PeterSalles@users.noreply.github.com>
2025-05-18 21:47:35 -0300

    Add files via upload

---
+++ Ex1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma senha:");
+        string senha = Console.ReadLine();
+
+        static bool VerificaSenha(string senha)
+        {
+            if (senha.Length < 8)
+                return false;
+            bool Maiuscula = Regex.IsMatch(senha, "[A-Z]");
+            bool Numero = Regex.IsMatch(senha, "[0-9]");
+            bool Especial = Regex.IsMatch(senha, "[!@#$%^&*(),.?\":{}|<>]");
+            return Maiuscula && Numero && Especial;
+        }
+    if (VerificaSenha(senha))
+        {
+            Console.WriteLine("Senha válida");
+        }
+        else
+        {
+            Console.WriteLine("Senha inválida");
+        }
+    }
+}
